0090a8b5c62b6b1b2e5cc3f877df9cd8f760bcfa
erieger11 <erieger530@gmail.com>
2024-03-14 15:02:21 -0400

    added something but then i deleted

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,5 +1,9 @@
 
 public class Bins {
+    Integer[] resultsLogged = new Integer[10];
+
+    Dice dice = new Dice();
+
 
 }
 //Ethan
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,12 @@
+import java.util.Random;
+
+
+
 public class Dice {
+    int dice1 = (int) (Math.random() * 6) + 1;
+    int dice2 = (int) (Math.random() * 6) + 1;
 
-
+    public int totalDiceRoll(){
+        return dice1 + dice2;
+    }
 }
